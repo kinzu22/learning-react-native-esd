4503e4694ea4e96975f5d1bda07089bf631c4455
nhattl <nhattl@hpt.vn>
2023-06-13 08:43:33 +0700

    Thêm tab navigator

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
-// import {SafeAreaView} from 'react-native';
+import {SafeAreaView, useColorScheme, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-
-// import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {RootNavigator} from './src/navigations/root.navigator';
 
 const App = () => {
-  // const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === 'dark';
 
-  // const backgroundStyle = {
-  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  // };
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider style={styles.container}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
 
 export default App;
--- src/navigations/tabs/tab.navigator.tsx
+++ src/navigations/tabs/tab.navigator.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import {AppRouter} from '../AppRouter';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import HomeScreen from '../../screens/home';
+import DashboardScreen from '../../screens/dashboard';
+import AssetScreen from '../../screens/asset';
+import ScanScreen from '../../screens/scan';
+import TaskScreen from '../../screens/task';
+import RequestScreen from '../../screens/request';
 
 const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name={AppRouter.Home} component={HomeScreen} />
-      {/* <Tab.Screen name={AppRouter.Dashboard} component={DashboardScreen} />
-      <Tab.Screen name={AppRouter.Asset} component={AssetScreen} /> */}
+      <Tab.Screen name={AppRouter.Dashboard} component={DashboardScreen} />
+      <Tab.Screen name={AppRouter.Asset} component={AssetScreen} />
+      <Tab.Screen name={AppRouter.Scan} component={ScanScreen} />
+      <Tab.Screen name={AppRouter.Task} component={TaskScreen} />
+      <Tab.Screen name={AppRouter.Request} component={RequestScreen} />
     </Tab.Navigator>
   );
 };
